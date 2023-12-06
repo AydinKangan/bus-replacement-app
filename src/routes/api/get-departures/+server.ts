f3133cfe0ae592ed299323a636f3fcc40cfb2881
Aydin Elhan <200030355@student.kangan.edu.au>
2023-12-06 14:20:09 +1100

    routes from each station being found

--- src/routes/api/get-departures/+server.ts
+++ src/routes/api/get-departures/+server.ts
@@ -9,21 +9,59 @@ export const POST: RequestHandler = async ({request}: any) => {
 
         const stopId = body.stopId.toString();
 
-        console.log(stopId)
+        const currentUtcTime = new Date().toISOString();
+
 
         const headers = {
             "X-Api-Key": X_API_KEY,
         };
 
-        const response: AxiosResponse = await axios.get(
+        const getDepartures: AxiosResponse = await axios.get(
             `https://ptvapiwrapper.azurewebsites.net/trains/get-departures/${stopId}`,
             { headers }
         );
 
+        const departures = getDepartures.data.departures;
 
-        return new Response(JSON.stringify(response.data), { status: 200 });
+        
+
+        const getRoutes: AxiosResponse = await axios.get(
+            `https://ptvapiwrapper.azurewebsites.net/trains/get-all-routes`,
+            { headers }
+        );
+
+
+        const routes = getRoutes.data;
+
+        const getDirections: AxiosResponse = await axios.get(
+            `https://ptvapiwrapper.azurewebsites.net/trains/get-directions`,
+            { headers }
+        );
+
+        const directions = getDirections.data;
+
+        const uniqueDepartureRoutes = await Array.from(new Set(departures.map((departure: any) => departure.route_id)));
+        console.log(uniqueDepartureRoutes);
+
+        const matchingDirections = directions.filter((direction: any) => uniqueDepartureRoutes.includes(direction.route_id));
+
+        const uniqueDirections = matchingDirections.reduce((acc: any[], direction: any) => {
+            const { direction_id, direction_name, route_id } = direction;
+            if (!acc.some((d: any) => d.direction_id === direction_id)) {
+                acc.push({ direction_id, direction_name, route_id });
+            }
+            return acc;
+        }, []);
+
+
+
+        // const viewingData = [departures[0], routes[0], matchingDirections[0]];
+
+        return new Response(JSON.stringify(uniqueDirections), { status: 200 });
     } catch (error) {
         console.error("Error parsing request body:", error);
         return new Response("Bad Request", { status: 400 });
     }
 };
+
+        
