d3d46f61ee060753edc79c03551b127e84ca5413
Aydin Elhan <200030355@student.kangan.edu.au>
2024-01-30 10:52:49 +1100

    fixed theme selector button

--- src/lib/theme-selector.svelte
+++ src/lib/theme-selector.svelte
@@ -1,46 +1,90 @@
 <script lang="ts">
   import supabase from "../routes/supabase";
 
-    export let user: any
+  import ptvIcon from "$lib/ptv-icon.jpeg"
+  export let user: any
+  let selectedTheme: any = {};
 
-    const themes: string[] = ["ptvTheme", "skeleton", "wintry", "modern", "hamlindigo", "rocket", "sahara", "gold-nouveau", "vintage", "seafoam", "crimson"];
+  const themes = [
+    { name: "ptvTheme", icon: "" },
+    { name: "skeleton", icon: "💀" },
+    { name: "wintry", icon: "🌨️" },
+    { name: "modern", icon: "🤖" },
+    { name: "hamlindigo", icon: "👔" },
+    { name: "rocket", icon: "🚀" },
+    { name: "sahara", icon: "🏜️" },
+    { name: "gold-nouveau", icon: "💫" },
+    { name: "vintage", icon: "📺" },
+    { name: "seafoam", icon: "🧜‍♀️" },
+    { name: "crimson", icon: "⭕" },
+  ];
 
-    const changeTheme = (theme: string) => {
-        if (user) {
-        const userId = user.data.user?.id;
-    supabase
+    
+
+  const getCurrentTheme = async () => {
+    let databaseTheme: string;
+    await supabase
       .from("user-data")
       .select("*")
-      .eq("user_id", userId)
+      .eq("user_id", user.data.user?.id)
       .then((res) => {
-        // console.log(res.data?.length);
         if (res.data?.length) {
-          // Update existing row
-          supabase
-            .from("user-data")
-            .update({
-              selected_theme: theme,
-            })
-            .eq("user_id", userId)
-            .then((res) => {
-                if (res.status >= 200 && res.status < 300) {
-              } else {
-                console.log(res);
-              }
-            });
-        } 
+          databaseTheme = res.data[0].selected_theme;
+          selectedTheme = themes.find((t) => t.name === databaseTheme);
+
+        }
+        else {
+          selectedTheme = themes.find((t) => t.name === "ptvTheme");
+        }
       });
-    }
+  }
+     
+  if (user) {
+    getCurrentTheme();
+  }
+
+    const changeTheme = (theme: string) => {
+      selectedTheme = themes.find((t) => t.name === theme);
+        if (user) {
+        const userId = user.data.user?.id;
+        supabase
+          .from("user-data")
+          .select("*")
+          .eq("user_id", userId)
+          .then((res) => {
+            if (res.data?.length) {
+              // Update existing row
+              supabase
+                .from("user-data")
+                .update({
+                  selected_theme: theme,
+                })
+                .eq("user_id", userId)
+                .then((res) => {
+                    if (res.status >= 200 && res.status < 300) {
+                  } else {
+                    console.log(res);
+                  }
+                });
+            } 
+          });
+        }
         document.body.setAttribute('data-theme', theme);
     }
 
 
 </script>
 
-<div class="max-w-[50%]">
+<div class="max-w-[50%] max-h-[15rem] grid grid-cols-3 gap-4 mt-[5.25rem]">
     {#each themes as theme}
-        <button class="btn variant-filled" on:click={() => changeTheme(theme)}>
-            {theme}
-        </button>
+      <button class="btn {theme.name === selectedTheme.name ? 'variant-filled-surface' : 'variant-soft'} rounded-xl w-16 h-10" on:click={() => changeTheme(theme.name)}>
+        <span class="flex items-center my-auto">
+          {#if theme.name === "ptvTheme"}
+            <img class="w-5 h-5" src={ptvIcon} alt="PTV Logo" />
+          {:else}
+            {theme.icon}
+          {/if}
+        </span>
+      </button>
     {/each}
 </div>
